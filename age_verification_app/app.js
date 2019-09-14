9093e26abfa22aa1eff1ed1ce4c7e3001614b8e6
Barbie <barbara.kelly892@gmail.com>
2019-09-14 16:40:53 -0400

    made age_verification_app functional

--- age_verification_app/app.js
+++ age_verification_app/app.js
@@ -1,34 +1,43 @@
 //App to test what activities users can do based on their age and the age restriction of that activity
-// users Sarah (32), Bob (19), Henry (22), Katie (9)
+// creates initial array of users
+var users = [
+	{name: "Sarah", age:32},
+	{name: "Bob", age:19},
+	{name: "Henry", age:22},
+	{name: "Katie", age:9},
+]
+// creates two arrays of activites which are age restricted.
+// adultActivities are those where you must be at least a certain age. 
+// childActivities are those where you must be under a certain age. 
+var adultActivities = [
+	{name: "driving", activity: "to drive", function: driving = function(age) {return age >= 16;}},
+	{name: "drinking", activity: "to drink", function: drinking = function(age) {return age >= 21;}}, 
+	{name: "renting", activity: "to rent a car", function: rentingCar = function(age) {return age >= 25;}},
+];
 
-var user1 = {name: "Sarah", age:32}
-var user2 = {name: "Bob", age:19}
-var user3 = {name: "Henry", age:22}
-var user4 = {name: "Katie", age:9}
-var users = [user1, user2, user3, user4]
+var childActivities = [
+	{name: "playing", activity: "to play in the ball pit", function: playingAtMcDonalds = function (age) {return age <10;}}
+];
 
-var driving = function (user) {
-	return user.age >= 16;	
-}
-
-var drinking = function (user) {
-	return user.age >= 21;
-}
-
-var rentCar = function (user) {
-	return user.age >= 25;
-}
-
-var playMcDonalds = function (user) {
-	return user.age < 10;
-}
-
-
-var toPrint = function() {
-	for (let i = 0; i < 4; i++) {
-		if (driving(users[i])) {
-			return users[i].name + " can drive";
+// pushes Abilities to users. 
+var givesAbilities = function() {
+	for (let i = 0; i < users.length; i++) {
+		for (let z = 0; z < adultActivities.length; z++) {
+			if (adultActivities[z].function(users[i].age)){
+				users[i][adultActivities[z].name] = users[i].name + " is old enough " + adultActivities[z].activity;
+			} else {
+				users[i][adultActivities[z].name] = users[i].name + " is not old enough " + adultActivities[z].activity;
+			}
+		}
+		for (let z = 0; z < childActivities.length; z++) {
+			if (childActivities[z].function(users[i].age)){
+				users[i][childActivities[z].name] = users[i].name + " is young enough " + childActivities[z].activity;
+			} else {
+				users[i][childActivities[z].name] = users[i].name + " is  not young enough " + childActivities[z].activity;
+			}
 		}
 	}
 }
-console.log(toPrint());
+givesAbilities();
+
+console.log(users);
