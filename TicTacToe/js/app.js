0d49a050c6723daef218d402382c46c280532a5e
Barbie <barbara.kelly892@gmail.com>
2019-10-12 10:34:21 -0400

    added spacing to TicTacToe game and removed Column/Row text from board

--- TicTacToe/js/app.js
+++ TicTacToe/js/app.js
@@ -15,28 +15,32 @@ var makeBoard = function(){
     playerTwoScore.innerHTML = playerTwoCounter;
     playerOne.className = "btn btn-primary";
     playerTwo.className = "btn btn-secondary";
+
     for(var row = 0; row < 3; row++){
         var rowElement = document.createElement("div");
         rowElement.className ="row";
         boardData[row] = [];
+
         for (var column=0; column < 3; column++){
             var columnElement = document.createElement("div");
             columnElement.className = "col text-center align-middle border board_piece"; 
             columnElement.id = row +"-" + column; 
-            columnElement.innerHTML = "Row " + row + " Column " + column; 
             columnElement.onclick = function(){
                 var idString = this.id;
                 var idArray= idString.split("-");
                 console.log(idArray);
+
                 if(this.innerHTML == getXPiece() || this.innerHTML == getOPiece()){   
                         alert("You cannot change this piece");
                 }
+
                 else if (currentPlayer == "Player 1") {
                     this.innerHTML = getXPiece();
                     checkWin(idArray[0], idArray[1], getXPiece());
                     currentPlayer = "Player 2";
                     playerOne.className = "btn btn-secondary";
                     playerTwo.className = "btn btn-primary";
+
                 } else if (currentPlayer == "Player 2"){
                     this.innerHTML = getOPiece();
                     checkWin(idArray[0], idArray[1], getOPiece());
@@ -45,6 +49,7 @@ var makeBoard = function(){
                     playerTwo.className = "btn btn-secondary";
                 }
             }
+
         boardData[row][column] = columnElement;
         rowElement.appendChild(columnElement);    
         }
@@ -97,19 +102,43 @@ var checkWinHorizontal = function(row, column, piece){
     }
 }
 
+var checkWinDiagonal = function(row, column, piece){
+    if(boardData[row-2] && boardData[row-2][column-2] && boardData[row-2][column-2].innerHTML == piece){
+        points++;
+        console.log(points);
+    }
+    if(boardData[row-1] && boardData[row-1][column-1] && boardData[row-1][column-1].innerHTML == piece){
+        points++;
+        console.log(points);
+    }
+    if(boardData[row+1] && boardData[row+1][column+1] && boardData[row+1][column+1].innerHTML == piece){
+        points++;
+        console.log(points);
+    }
+    if(boardData[row+2] && boardData[row+2][column+2] && boardData[row+2][column+2].innerHTML == piece){
+        points++;
+        console.log(points);
+    }
+}
+
+
 
 var checkWin = function(row, column, piece){
     points = 0;
     checkWinVertical(row, column, piece);
     checkWinHorizontal(row, column, piece);
+    checkWinDiagonal(row, column, piece);
     if (points >=2) {
         alert("YOU WIN " + currentPlayer);
+
         if(currentPlayer == "Player 1") {
             playerOneCounter += 1;
         }
+
         else if(currentPlayer == "Player 2") {
             playerTwoCounter += 1;
         }
+
         boardElement.innerHTML = "";
         boardData = [];
         makeBoard();
@@ -119,8 +148,8 @@ var checkWin = function(row, column, piece){
 
 resetButton.onclick = function() {
     boardElement.innerHTML = "";
-        boardData = [];
-        makeBoard();
+    boardData = [];
+    makeBoard();
 }
 
 
